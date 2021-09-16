a868643dfafc767d58d8428e671708b6c5b60191
Reinhardt <reinhardt.cgr@gmail.com>
2021-09-16 17:15:49 +1200

    Have chakra install

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box } from "@chakra-ui/react"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
+      <Box bg="#EDF2F7" width="100%" height="100%" padding={5} color="darkgrey">
         <h2>Window</h2>
         <div>
             <p>Total Files: 5</p>
             <p>Total Filesize: 921MB</p>
         </div>
-      </div>
+      </Box>
     </div>
   );
 }
