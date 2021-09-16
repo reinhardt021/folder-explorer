657f55d36d047e1d9872d40000b4ec82b5926553
Reinhardt <reinhardt.cgr@gmail.com>
2021-09-16 17:55:02 +1200

    added some more static content to fix the UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { 
     Box,
+    Circle,
     Container,
+    HStack,
     Text
 } from "@chakra-ui/react"
 
@@ -12,7 +14,7 @@ function App() {
         bgColor="gray.200" 
         width="100%" 
         height="100%" 
-        padding={5} 
+        padding="20px" 
         color="gray.500"
       >
         <Text align="left">
@@ -22,13 +24,30 @@ function App() {
           boxShadow="base"
           rounded="lg"
         >
-            <Box bgColor="gray.400">
-                <p>Add circles here</p>
+            <Box bgColor="gray.400" align="left" padding="5px" paddingLeft="10px">
+                <HStack>
+                    <Circle size="15px" bgColor="gray.500"></Circle>
+                    <Circle size="15px" bgColor="gray.500"></Circle>
+                    <Circle size="15px" bgColor="gray.500"></Circle>
+                </HStack>
             </Box>
-            <Box bgColor="gray.50">
+            <Box bgColor="gray.50" padding="40px">
                 <Container align="left">
-                    <p>Total Files: 5</p>
-                    <p>Total Filesize: 921MB</p>
+                    <Box paddingBottom="30px">
+                        <Container>
+                            Bikes
+                        </Container>
+                        <Container>
+                            Planes
+                        </Container>
+                        <Container>
+                            Cars
+                        </Container>
+                    </Box>
+                    <Box borderTop="1px" borderColor="gray.400" paddingTop="20px">
+                        <p>Total Files: 5</p>
+                        <p>Total Filesize: 921MB</p>
+                    </Box>
                 </Container>
             </Box>
         </Box>
