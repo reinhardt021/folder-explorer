fdb434960a123eddd3c21893134478b056e31cf4
Reinhardt <reinhardt.cgr@gmail.com>
2021-09-16 17:35:02 +1200

    added more boxes for the UI to look closer to requirements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-import { Box } from "@chakra-ui/react"
+import { 
+    Box,
+    Container,
+    Text
+} from "@chakra-ui/react"
 
 function App() {
   return (
     <div className="App">
-      <Box bg="#EDF2F7" width="100%" height="100%" padding={5} color="darkgrey">
-        <h2>Window</h2>
-        <div>
-            <p>Total Files: 5</p>
-            <p>Total Filesize: 921MB</p>
-        </div>
+      <Box 
+        bgColor="gray.200" 
+        width="100%" 
+        height="100%" 
+        padding={5} 
+        color="gray.500"
+      >
+        <Text align="left">
+            Window
+        </Text>
+        <Box 
+          boxShadow="base"
+          rounded="lg"
+        >
+            <Box bgColor="gray.400">
+                <p>Add circles here</p>
+            </Box>
+            <Box bgColor="gray.50">
+                <Container align="left">
+                    <p>Total Files: 5</p>
+                    <p>Total Filesize: 921MB</p>
+                </Container>
+            </Box>
+        </Box>
       </Box>
     </div>
   );
